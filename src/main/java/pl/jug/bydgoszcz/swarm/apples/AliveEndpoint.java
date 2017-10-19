482f9bb5597e1da3bc9668b9f9ac830039a19cfc
Michal Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2017-10-19 12:01:18 +0200

    consul

--- src/main/java/pl/jug/bydgoszcz/swarm/apples/AliveEndpoint.java
+++ src/main/java/pl/jug/bydgoszcz/swarm/apples/AliveEndpoint.java
@@ -1,5 +1,7 @@
 package pl.jug.bydgoszcz.swarm.apples;
 
+import org.wildfly.swarm.topology.Advertise;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
@@ -10,6 +12,7 @@ import javax.ws.rs.core.Response;
  * Date: 10/18/17
  */
 @Path("/alive")
+@Advertise(value = "apple-service", tags = {"rest", "swarm", "apples"})
 public class AliveEndpoint {
     @GET
     public Response isAlive() {
